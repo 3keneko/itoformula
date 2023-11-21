988ea2448cec76a78209e0b4d93d5b030008fc2e
3keneko <lucaslej19@gmail.com>
2023-11-21 19:33:11 +0100

    Phe

---
+++ pycode/brown_mot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def brownian_motion(timesteps, dt):
+    """Generate a 1D Brownian motion, this is the simple bachelier model with no drift."""
+    t = np.linspace(0.0, 10.0, timesteps)
+    W = np.cumsum(np.random.randn(timesteps))
+    return t, W
+
+def brownian_motion_var_and_drift(timesteps, dt, var, drift):
+    """Generate a 1D Brownian with drift and instantaneous variance"""
+    t = np.linspace(0.0, 10.0, timesteps)
+    W = np.cumsum(drift*dt + var*np.random.randn(timesteps))
+    return t, W
+
+# Parameters
+timesteps = 1000  # Number of time steps
+dt = 0.01         # Time step size
+
+# Generate three instances of Brownian motion
+t1, W1 = brownian_motion_var_and_drift(timesteps, dt, 1, 10)
+t2, W2 = brownian_motion_var_and_drift(timesteps, dt, 1, 10)
+t3, W3 = brownian_motion_var_and_drift(timesteps, dt, 1, 10)
+
+# Plot the Brownian motion
+plt.figure(figsize=(10, 6))
+plt.plot(t1, W1, label='1')
+# plt.plot(t2, W2, label='2')
+# plt.plot(t3, W3, label='3')
+#plt.plot(t1, 0.2 * np.ones(timesteps), label="A")
+# Customize the plot
+# plt.title('Brownian Motion')
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ pycode/option_call.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def geometric_brownian_motion(T, mu, sigma, S0, n, M):
+    """
+    Generate geometric Brownian motion paths.
+
+    Parameters:
+        T (float): Total time in years.
+        mu (float): Drift (average return).
+        sigma (float): Volatility (standard deviation of return).
+        S0 (float): Initial value of the process.
+        dt (float): Time step size.
+
+    Returns:
+        numpy.ndarray: Array of GBM values over time.
+    """
+    dt = T / n
+    St = np.exp(
+        (mu - 0.5 * sigma**2) * dt
+        + sigma * np.random.normal(0, np.sqrt(dt), size=(M, n)).T
+    )
+    St = np.vstack([np.ones(M), St])
+    St = S0 * St.cumprod(axis=0)
+
+    return St
+
+# Parameters
+T = 2.0  # Total time in years
+mu = 0.1  # Drift
+sigma = 0.3  # Volatility
+S0 = 100.0  # Initial value
+steps = 1000 # number of steps
+M = 1 # number of simulations
+# Generate GBM path
+St = geometric_brownian_motion(T, mu, sigma, S0, steps, M)
+time = np.linspace(0, T, steps+1)
+
+tt = np.full(shape=(M, steps+1), fill_value=time).T
+# Plot the results
+plt.plot(tt, 130 * np.ones(steps+1), label="K")
+plt.plot(tt, St)
+plt.title('Strike vs asset price')
+plt.xlabel('Time (Years)')
+plt.ylabel('Asset Price')
+plt.show()
